933d8e7768465c6a8ce6484bd03b145485dfcc70
Zeke <hnta3574@gmail.com>
2019-09-21 20:23:02 +0900

    ゲームシーン遷移スクリプト(仮)

---
+++ Monkey/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{    
+    void Start()
+    {
+        var canvas = GameObject.Find("FadeCanvas");
+        canvas.GetComponent<Fade>().FadeIn();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            var canvas = GameObject.Find("FadeCanvas");                         
+            canvas.GetComponent<Fade>().FadeOut();
+            Invoke("SceneTransition",canvas.GetComponent<Fade>().speed);
+        }    
+    }
+
+    void SceneTransition(){
+        SceneManager.LoadScene("Game");
+    }
+}
