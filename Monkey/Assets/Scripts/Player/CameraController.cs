f7b2a2215ec788d1a451ce66623becec21c8ef3a
Reki Yamashita <chloir.sharp@gmail.com>
2019-09-22 00:04:05 +0900

    UniRx追加、移動、ガード（致命的バグあり、明日直します(°▽°)）実装（ひたすら弾を吐くダミーも追加）

---
+++ Monkey/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject cameraTarget = null;
+    private float duration = 0.6f;
+    
+    void Start()
+    {
+        cameraTarget = GameObject.FindGameObjectWithTag("CameraTarget");
+    }
+
+    void Update()
+    {
+        var tempPosition = transform.position;
+
+        transform.position = cameraTarget.transform.position;
+    }
+}
