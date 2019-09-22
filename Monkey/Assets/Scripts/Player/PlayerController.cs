3b3ca5cf9376dcc855f30e4c674d1df017688127
Reki Yamashita <chloir.sharp@gmail.com>
2019-09-22 11:41:27 +0900

    ゲームパッドによる移動、ガードを実装、モデル、モーション導入

--- Monkey/Assets/Scripts/Player/PlayerController.cs
+++ Monkey/Assets/Scripts/Player/PlayerController.cs
@@ -9,15 +9,24 @@ public class PlayerController : MonoBehaviour
 {
     private bool isGuarding = false;
     private bool isJustGuard = false;
+    private bool hit = false;
+    private int justGuardDuration = 10;
+    private int timer = 0;
     private float movementVelocity = 2;
     private float duration = 0.3f;
     private float targetPosition;
+    private Animator animator = null;
+    private int dash = Animator.StringToHash("dashTrigger");
+    private int guard = Animator.StringToHash("guard");
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+
         this.OnTriggerEnterAsObservable()
             .Subscribe(x =>
             {
+                hit = true;
                 if (isJustGuard)
                 {
                     Debug.Log("Just Guard");
@@ -27,7 +36,7 @@ public class PlayerController : MonoBehaviour
                     Debug.Log("Guard");
                 }
 
-                if (!isGuarding || !isJustGuard)
+                if (!isGuarding && !isJustGuard)
                 {
                     Debug.Log("Dead");
                 }
@@ -41,22 +50,50 @@ public class PlayerController : MonoBehaviour
         var tempVec = transform.position;
         var tempPosition = transform.position.x;
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetButton("Guard"))
         {
             isGuarding = true;
+            animator.SetBool(guard, true);
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetButtonUp("Guard"))
+        {
+            StartCoroutine(JustGuard());
+        }
+
+        if (Input.GetButtonDown("D-Right")&& !isGuarding)
+        {
+            targetPosition = tempPosition - movementVelocity;
+            animator.SetTrigger(dash);
+        }
+
+        if (Input.GetButtonDown("D-Left") && !isGuarding)
         {
             targetPosition = tempPosition + movementVelocity;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            targetPosition = tempPosition - movementVelocity;
-        }
-
         tempVec.x = Mathf.Lerp(tempPosition, targetPosition, duration);
         transform.position = tempVec;
+    }
+
+    IEnumerator JustGuard()
+    {
+        isJustGuard = true;
+        for (timer = 0; timer < justGuardDuration; timer++)
+        {
+            if (hit)
+            {
+                yield return null;
+                animator.SetBool(guard, false);
+                isJustGuard = false;
+                isGuarding = false;
+                yield break;
+            }
+            yield return null;
+        }
+
+        animator.SetBool(guard, false);
+        isJustGuard = false;
+        isGuarding = false;
     }
 }
