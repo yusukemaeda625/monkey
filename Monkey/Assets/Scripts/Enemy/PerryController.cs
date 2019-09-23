0c1909a1e949dfecfec71755b161cc58dfdd346d
Zeke <hnta3574@gmail.com>
2019-09-23 15:13:43 +0900

    タイトル追加

--- Monkey/Assets/Scripts/Enemy/PerryController.cs
+++ Monkey/Assets/Scripts/Enemy/PerryController.cs
@@ -12,22 +12,31 @@ public class PerryController : MonoBehaviour
     private float timer = 0f;
     private float addForceParam = 0f;
     public float SkillInterval = 10f;
+
+    [SerializeField] float battleDistance = 10f;
     [SerializeField] float rainRate = 0.3f;
     [SerializeField] GameObject player;
 
     [SerializeField] float kickField = 1f;
     [SerializeField] float kickPower = 3f;
-    void Start()
+
+    [SerializeField] int numCannonRash = 4;
+    [SerializeField] float cannonRashInterval = 0.4f;
+
+    public bool isBattle = false;
+
+     void Start()
     {        
         player = GameObject.Find("Player");
+        timer = SkillInterval - 1;
     }
     
     void Update()
     {                
         if(hp == 0){
             //死ぬ処理
-            
-        }
+            Destroy(this.gameObject);
+        }        
         var ve = player.transform.position - this.transform.position;
 
         if(ve.magnitude <= kickField){
@@ -35,6 +44,8 @@ public class PerryController : MonoBehaviour
             return;
         }
 
+        if(ve.magnitude <= battleDistance){
+            isBattle = true;            
         if(timer >= SkillInterval){
             timer = 0f;
 
@@ -42,30 +53,66 @@ public class PerryController : MonoBehaviour
             case 1 :
             {
                 //第三形態
+                var r = Random.Range(0f,10f);
+                Debug.Log(r);
+                if(r >= 5f){
+                    cannonRashInterval = Random.Range(0.2f,0.5f);
+                    RandomCannonRash();
+                }else{
+                    CannonRain();
+                }    
             }
             break;
             case 2 :
             {
                 //第二形態
-                CannonRain();
+                var r = Random.Range(0f,10f);
+                Debug.Log(r);
+                if(r >= 5f){
+                    RandomCannonRash();
+                }else{
+                    CannonRain();
+                }               
             }
             break;
             case 3 :
             {
-                //初期状態
-                Cannon();                
+                //初期状態                
+                Cannon();
             }
             break;
         }        
         }
+        }
         timer += Time.deltaTime;
     }
 
-    //キャノンラッシュ
+
     void Cannon(){
         Invoke("ShotCannon",0f);
         Invoke("ShotCannon",0.5f);
         Invoke("ShotBigCannon",1f);
+    }
+
+    void RandomCannonRash(){
+        var t = 0f;
+        for(int i = 0; i < numCannonRash; i++){
+            var r = Random.Range(0f,10f);
+            if(r >= 3f){
+                Invoke("ShotCannon",t);
+            }else{
+                Invoke("ShotBigCannon",t);
+            }         
+            t += cannonRashInterval;   
+        }
+    }
+
+    void NormalCannonRash(){
+        var t = 0f;
+        for(int i = 0; i < numCannonRash; i++){
+            Invoke("ShotCannon",t);
+            t += cannonRashInterval;
+        }
     }
     
     void CannonRain(){
@@ -78,12 +125,13 @@ public class PerryController : MonoBehaviour
     }
     
     void Kick(){        
+        Damage();
         Debug.Log("Perrys Kick");
         player.GetComponent<PlayerController>().KnockBackPlayer(-kickPower);
     }
 
     void Gatling(){
-
+        //float shotRate = 
     }
 
     private void ShotBullet(){
--- Monkey/Assets/Scripts/Enemy/RainCannon.cs
+++ Monkey/Assets/Scripts/Enemy/RainCannon.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class RainCannon : MonoBehaviour
 {
-    [SerializeField] float xmin = -50f;
-    [SerializeField] float xmax = -30f;
 
-    [SerializeField] float ymin = 30f;
-    [SerializeField] float ymax = 30f;
+    [SerializeField] float Pos1 = 20;
+    [SerializeField] float Pos2 = 30;
+    [SerializeField] float Pos3  = 40;
 
-    private Vector3 force;
+    [SerializeField] float UpPower = 35f;
+    [SerializeField] Vector3 force;        
     // Start is called before the first frame update
     void Start()
-    {
-        force = new Vector3(Random.Range(xmin,xmax),Random.Range(ymin,ymax),0);
+    {        
+        var r = Random.Range(0f,10f);
+        if(r <= 3f){
+            force = new Vector3(-Pos1,UpPower,0);
+        }else if(r <= 6f){
+            force = new Vector3(-Pos2,UpPower,0);
+        }else{
+            force = new Vector3(-Pos3,UpPower,0);
+        }                
         GetComponent<Rigidbody>().AddForce(force);
     }
 
@@ -25,7 +32,7 @@ public class RainCannon : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider col){
-        if(col.tag != "Enemy"){
+        if(col.tag != "Enemy" && col.tag != "Bullet"){
             Destroy(this.gameObject);
         }
     }
